211b5c48dc148e7a2602fbd9cbc227bbdcb6b61d
maxrichardburgess <maxrichardburgess@users.noreply.github.com>
2020-10-11 18:26:37 +1100

    Created herokuapp

---
+++ coles_bay_campsites_app/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for coles_bay_campsites_app project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coles_bay_campsites_app.settings')
+
+application = get_asgi_application()
