26aae35e076c6f8e344ca7b30609e50fc9a44216
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-10-26 18:43:37 +1100

    encoded instead of decoded

--- coles_bay_campsites_app/views.py
+++ coles_bay_campsites_app/views.py
@@ -372,7 +372,7 @@ def viewcamp(request):
     <a href="https://www.maxstuff.net">My Other Work</a>
 </p>
 <script>
-    review_link_two.href = btoa(\""""+google_form_b64+"""\");
+    review_link_two.href = atob(\""""+google_form_b64+"""\");
     review_link_one.href = review_link_two.href;
 </script>
 </body>""")
